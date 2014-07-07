f2fad674b8d8a41f24dbf236ab20d9e5b509a26c
bornreddy <marisa.reddy@gmail.com>
2014-07-07 12:57:59 -0400

    writing ppm from marisa's computer

--- PIL_data.py
+++ PIL_data.py
@@ -43,8 +43,10 @@ def return_image(img,filename="output.jpg"):
     data=np.real(np.around(data))
     img=np.real(np.around(img))
     data=np.asarray(img)
-
     im=Image.fromarray(data)
+    #im.show()
+    print im.mode
+    im = im.convert('L')
     #im.show()
     im.save(filename,"JPEG")
 
@@ -60,8 +62,135 @@ def normalize(matrix, maxcolor=255., mincolor=0,):
   #print matrix_max, matrix_min
   a=(mincolor-maxcolor)/(matrix_min-matrix_max)
   b=(matrix_min*maxcolor-matrix_max*mincolor)/(matrix_min-matrix_max)
-  return a*matrix+b
+  return a*matrix+b, a, b
   #for row in matrix:
   #  for column in row:
   #    matrix[row][column]*=n_factor
-  #return matrix  
+  #return matrix 
+
+def denormalize(matrix,a,b):
+  return (matrix-b)/a
+
+
+#takes in ppm filename
+def ppm2mn(ppmFile="lena.ppm"):
+  img = Image.open(ppmFile)
+  img = img.convert('L')
+  width,height = img.size
+  data = list(img.getdata())
+  # data_mat = [[data[i] for i in range(n*width,(n+1)*height)] for n in range(height)]
+  data_mat = [data[n*width:(n+1)*width] for n in range(height)]
+  
+  f = open(ppmFile[:-4]+".mn",'w+')
+  f.write(str(width)); f.write("\n"); f.write(str(height))
+  f.write('\n')
+  for row in data_mat:
+    for column in row:
+      f.write(str(column))
+      f.write(" ")
+    f.write(str("\n"))
+    #f.write('\n')
+  f.close()
+  #this is a guess
+
+def read_mn(filename="lena.mn"):
+  f = open(filename)
+  width = int(f.readline())
+  height = int(f.readline())
+  data = np.zeros((width, height))
+  for i in range(height):
+    data[i] = np.array(map(int,f.readline().split()))
+  return np.array(data)
+
+def write_mnc(filename,data_real, data_imag,original_size,norm_params):
+  f = open(filename,"w+")
+  f.write(str(original_size[0])); f.write('\n'); f.write(str(original_size[1]))
+  f.write('\n')
+  for i in norm_params:
+    f.write(str(i) + "\n")
+  for row in data_real:
+    for column in row:
+      f.write(str(int(np.around(column))))
+      f.write(" ")
+    f.write(str("\n"))
+  for row in data_imag:
+    for column in row:
+      f.write(str(int(np.around(column))))
+      f.write(" ")
+    f.write(str("\n"))
+    #f.write('\n')
+  f.close()
+
+def read_mnc(filename="lena.mnc"):
+  f = open(filename,"r+");
+  original_width = int(f.readline())
+  original_height = int(f.readline())
+  original_size = (original_width,original_height)
+  a_real = float(f.readline())
+  b_real = float(f.readline())
+  a_imag = float(f.readline())
+  b_imag = float(f.readline())
+  big_array = []
+  line = f.readline()
+  while line:
+    array_line = [int(i) for i in line.split()]
+    big_array.append(array_line)
+    line = f.readline()
+  height = len(big_array)
+  # denormalizing real and imag arrays
+  real_array = np.array(big_array[:height/2])
+  real_array = (real_array-b_real)/a_real
+  imag_array = np.array(big_array[height/2:])
+  imag_array = (imag_array-b_imag)/a_imag
+  return real_array + 1j*imag_array,original_size
+
+def write_mn(data, filename="output_lena"):
+  # clean up data to satisfy ppm requirements
+  data = [[int(data[i][j]) for j in range(len(data[0]))] for i in range(len(data))]
+  width = len(data[0])
+  height = len(data)
+  for i in range(width):
+    for j in range(height):
+      if data[j][i] > 255:
+        data[j][i] = 255
+      elif data[j][i] < 0:
+        data[j][i] = 0
+  print data
+  f = open(filename+".mn",'w+')
+  f.write(str(len(data[0]))); f.write("\n"); f.write(str(len(data)))
+  f.write('\n')
+  for row in data:
+    for column in row:
+      f.write(str(column))
+      f.write(" ")
+    f.write(str("\n"))
+    #f.write('\n')
+  f.close()
+
+def write_ppm(data, filename="output_lena"):
+  # clean up data
+  data = [[int(data[i][j]) for j in range(len(data[0]))] for i in range(len(data))]
+  width = len(data[0])
+  height = len(data)
+  for i in range(width):
+    for j in range(height):
+      if data[j][i] > 255:
+        data[j][i] = 255
+      elif data[j][i] < 0:
+        data[j][i] = 0
+  print data
+  f = open(filename+".ppm",'w+')
+  f.write("P3\n")
+  f.write(str(width) + " " + str(height) + "\n")
+  f.write("255\n")
+  for row in data:
+    for column in row:
+      out_string = (str(column) + " ")*3
+      f.write(out_string)
+  f.close()
+
+
+# ppm2mn()
+# data = read_mn()
+# print data[0]
+  #get size, 
--- compression.py
+++ compression.py
@@ -1,9 +1,11 @@
 from PIL_data import *
 from FFT import *
+from cmath import *
 COMPRESSION_PERCENT = 10
-COMPRESSION_FACTOR = 1 #must be a power of two
+COMPRESSION_FACTOR = 2 #must be a power of two
 
-def compress(filename = "images/small.jpg"):
+def jpg_compress(filename = "images/small.jpg"):
+  filename = "images/tiger.jpg"
   img = get_image(filename)
   #img=np.array([[img[x][y] for y in range(8)] for x in range(8)])
   #print img
@@ -15,15 +17,53 @@ def compress(filename = "images/small.jpg"):
   FFT_img = two_d_FFT(img)
   #new_width = (len(FFT_img[0])*(100-COMPRESSION_PERCENT))/100
   #new_height = (len(FFT_img)*(100-COMPRESSION_PERCENT))/100
-  new_height=len(FFT_img[0])/COMPRESSION_FACTOR
-  new_width=len(FFT_img)/COMPRESSION_FACTOR
-
+  new_width=len(FFT_img[0])/COMPRESSION_FACTOR
+  new_height=len(FFT_img)/COMPRESSION_FACTOR
   #print new_width, new_height
-  #compressed_FFT_img = [[FFT_img[i][j] for i in range(new_height)] for j in range(new_width)]
-  compressed_img=np.real(two_d_iFFT(FFT_img))
+  compressed_FFT_img = np.zeros((new_width, new_height))
+  compressed_FFT_img = [[FFT_img[j][i] for i in range(new_height)] for j in range(new_width)]
+  compressed_img=np.real(two_d_iFFT(compressed_FFT_img))
   #print "dims of im", len(img),len(img[0])
   #print "dims of new img", len(compressed_img),len(compressed_img[0]),type(compressed_img)
-  np.around(compressed_img)
-  #return_image(compressed_img)
+  # np.around(compressed_img) #doesn't mutate 
+  return_image(compressed_img)
+
+def compress(filename="lena.mn",compression_factor=1):
+  data = read_mn(filename)
+  transformed_data = two_d_FFT(data)
+  # print transformed_data[0][0:4]
+  width,height = data.shape
+  new_width = int(round(compression_factor*width))
+  new_height = int(round(compression_factor*height))
+  compressed_data = [[transformed_data[i][j] for j in range(new_width)] for i in range(new_height)]
   
-compress()
+  #splitting up complex numbers and normalizing both resulting matrices
+  compressed_real,a_real,b_real = normalize(np.real(compressed_data))
+  compressed_imag, a_imag, b_imag = normalize(np.imag(compressed_data))
+  # compressed_normalized = np.around(compressed_real + 1j*(compressed_imag))
+  write_mnc(filename+"c",compressed_real,compressed_imag, (width,height),(a_real,b_real,a_imag,b_imag))
+
+def decompress(filename="lena.mnc"):
+  data, original_size = read_mnc(filename)
+  width, height = data.shape
+  extended_width = original_size[0] - width
+  extended_height = original_size[1] - height
+  row_pad = np.zeros(extended_width)
+  data = [np.hstack((row,row_pad)) for row in data]
+  col_pad = np.zeros((extended_height,original_size[0]))
+  data = np.vstack((data,col_pad))
+  print data.shape
+  iFFT_data = two_d_iFFT(data).real
+  print iFFT_data[0]
+  return iFFT_data
+
+
+
+# iFFT_data = decompress()
+
+# write_mn(iFFT_data)
+
+
+
+
+# compress()
